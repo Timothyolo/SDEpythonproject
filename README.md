d8c02dd3ac2bbdb67d7843bac96bad9764a0b4af
Timothyolo <97767706+Timothyolo@users.noreply.github.com>
2022-03-11 00:51:22 -0600

    admin functions

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # SDEpythonproject
+
+This is a bootcamp admin system. where we focus more on the Admin and its functions.
+
+## run project app
+
+windows
+py -3 sch.py
+
+Linux / Mac OS
+python3 sch.py
+
+## Built language
+
+this project was built using python3.
+
+## Author
+
+Timothy Olowookere
--- sch.py
+++ sch.py
@@ -1,4 +1,38 @@
+#This is my main function with infinite loop
+def home():
+    while 1:
+        print("welcome to the TLG bootcamp")
+        print("")
+        print("1. Login as student")
+        print("2. Login as teacher")
+        print("3. Login as admin")
 
+        user_option = input(str("Option : "))
+        if user_option == "1":
+            print("Student login")
+        elif user_option == "2":
+            print("Teacher login")
+        elif user_option == "3":
+            auth_admin()  # Linked to the function decleared below once authenticated
+        else:
+            print("No valid option was selected")
+
+# To authenticte a valid admin credentials.
+def auth_admin():
+    print("")
+    print("Admin Login")
+    print("")
+    username = input(str("Username : "))
+    password = input(str("Password : "))
+    if username == "admin":
+        if password == "password":
+            admin_session()
+        else:
+            print("incorrect password !")
+    else:
+            print("Login details not recognised")
+
+#These are what the admin can do while on session
 def admin_session():
     while 1:
         print("")
@@ -28,36 +62,23 @@ def admin_session():
             
             print(username + " has been registered as a teacher")
 
-def auth_admin():
-    print("")
-    print("Admin Login")
-    print("")
-    username = input(str("Username : "))
-    password = input(str("Password : "))
-    if username == "admin":
-        if password == "password":
-            admin_session()
-        else:
-            print("incorrect password !")
-    else:
-            print("Login details not recognised")
-
-def home():
-    while 1:
-        print("welcome to the college system")
-        print("")
-        print("1. Login as student")
-        print("2. Login as teacher")
-        print("3. Login as admin")
-
-        user_option = input(str("Option : "))
-        if user_option == "1":
-            print("Student login")
-        elif user_option == "2":
-            print("Teacher login")
         elif user_option == "3":
-            auth_admin()
+            print("")
+            print("Delete Existing student Account")
+            username = input(str("Student Username : "))
+            query_vals = (username, "student")
+            print(username + " has been deleted")
+            
+        elif user_option == "4":
+            print("")
+            print("Delete Existing Teacher Account")
+            username = input(str("Teacher Username : "))
+            query_vals = (username, "teacher")            
+            print(username + " has been deleted") 
+        
+        elif user_option == "5":
+            break
         else:
-            print("No valid option was selected")
+            print("you did not select a valid option")
 
 home()
